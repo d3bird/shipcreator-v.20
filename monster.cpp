985d185ed7d1fecb77cc926bb150e972578717bb
Scott Crawford <dogbird9@gmail.com>
2019-04-03 16:10:18 -0400

    added framework for life

---
+++ monster.cpp
@@ -0,0 +1,18 @@
+#include "monster.h"
+#include <string>
+
+
+monster::monster(){
+	name = "";
+	Rdam = 0;
+	Mdam = 0;
+	id = 'X';
+	hp = 10;
+
+}
+
+
+monster::~monster(){
+
+
+}
---
+++ monster.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+class monster
+{
+public:
+	monster();
+	~monster();
+
+private:
+	int hp;
+	int Mdam;
+	int Rdam;
+	std::string name;
+	char id;
+};
+
