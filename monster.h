a9a371c0e748f60265b4912328c9063a18545492
Scott Crawford <dogbird9@gmail.com>
2019-04-06 10:08:59 -0400

    fleshed out creature object

--- monster.h
+++ monster.h
@@ -3,13 +3,39 @@
 class monster
 {
 public:
-	monster();
-	~monster();
+	monster(char i){
+		xloc = 0;
+		floc = 0;
+		yloc = 0;
+		ws = 10;
+		Mdam = 5;
+		Rdam = 0;
+		hp = 10;
+		maxhp = 10;
+	}
+
+	~monster(){}
+
+	void setws(int i) { ws = i; }
+	int getws() { return ws; }
+
+	void setf(int t) { floc = t; }
+	void setx(int t) { xloc = t; }
+	void sety(int t) { yloc = t; }
+
+	int getx() { return xloc; }
+	int gety() { return yloc; }
+	int getf() { return floc; }
 
 private:
+	int xloc;
+	int floc;
+	int yloc;
+	int maxhp;
 	int hp;
 	int Mdam;
 	int Rdam;
+	int ws;// how fast it works in a room
 	std::string name;
 	char id;
 };
